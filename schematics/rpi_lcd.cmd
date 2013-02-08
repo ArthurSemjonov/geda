0758b8724850139034e0d0f6477c67827c2e0d21
Andrew Orr <aorr@iders.ca>
2013-02-08 00:51:43 -0600

    Added rpi lcd schematic

---
+++ schematics/rpi_lcd.cmd
@@ -0,0 +1,11 @@
+# Pin name action command file
+
+# Start of element LCD_CONN
+ChangePinName(LCD_CONN, 7, 7)
+ChangePinName(LCD_CONN, 5, 5)
+ChangePinName(LCD_CONN, 3, 3)
+ChangePinName(LCD_CONN, 1, 1)
+ChangePinName(LCD_CONN, 8, 8)
+ChangePinName(LCD_CONN, 6, 6)
+ChangePinName(LCD_CONN, 4, 4)
+ChangePinName(LCD_CONN, 2, 2)
